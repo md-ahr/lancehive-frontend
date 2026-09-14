5c08e0ee38cd3abf4d94f1614ee4d7ddbf14d45f
Abdul Halim <ahr.web.pro@gmail.com>
2026-09-14 13:19:52 +0600

    feat: Add Vitest configuration and Husky hooks, update .gitignore and package.json scripts

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [react({ compiler: true }), tailwindcss()],
+  test: {
+    passWithNoTests: true,
+  },
+})
